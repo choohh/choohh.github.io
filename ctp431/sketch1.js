14d3ff3605acc4d79a11158764ca6b9fbc73e0b6
choohh <chhh98@kaist.ac.kr>
2017-10-25 18:45:58 +0900

    newnew

--- ctp431/sketch1.js
+++ ctp431/sketch1.js
@@ -44,11 +44,14 @@ function draw() {
   for (j = 0; j < 5; j++) {
     line(cx+le*sin(18+36*j), cy-le*cos(18+36*j), cx-le*sin(18+36*j), cy+le*cos(18+36*j))
   }
-  push();
+
   stroke(rgb(0,0,0));
+  strokeWeight(4);
   line(cx, cy, cx+100*sin(18), cy-100*cos(18));
-  pop();
-  
+
+  stroke(lineColor);
+  strokeWeight(lineWeight);
+
   soundanl();
 
   var c = color(decagonColor);
